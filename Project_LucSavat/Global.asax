9177473e6bf2a20c0b651b8c420c2b360379b1a8
Kevin <Kevin@DESKTOP-KEVIN>
2017-02-12 17:12:23 +0100

    Add project files.

---
+++ Project_LucSavat/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Project_LucSavat.MvcApplication" Language="C#" %>
---
+++ Project_LucSavat/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Project_LucSavat.Startup))]
+namespace Project_LucSavat
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
